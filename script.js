50cb61ee899bfae30e9eab7857087dfea45d98cc
ArmParanyan <aparanyan472@gmail.com>
2023-09-16 17:09:39 +0400

    flipped on dogs

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
 const hamburger = document.getElementById('open-when-clicking');
 const menu_section = document.getElementById('menu-section');
-const close_menu = document.querySelector('.close-image');
+const close_menu_button = document.querySelector('.close-image');
+const header = document.querySelector('header');
+const main_section = document.getElementById('main-section');
+const body = document.querySelector('body');
+const central_div = document.querySelector('.central-div');
 
-
-hamburger.addEventListener('click', () => {
+function open_menu() {
     menu_section.style.display = 'block';
-});
 
-close_menu.addEventListener('click', () => {
+    // header.style.background = 'rgba(0, 0, 0, 0.80)';
+
+
+    // main_section.style.background = 'rgba(0, 0, 0, 0.8)';
+    // central_div.style.opacity = 0.2
+    ;
+
+    // main_section.style.zIndex = 3;
+
+    // central_div.style.background = 'rgba(0, 0, 0, 0.80)';
+
+
+    body.style.overflow = 'hidden';
+}
+
+
+hamburger.addEventListener('click', open_menu)
+
+
+function close_menu() {
     menu_section.style.display = 'none'
-});
+
+
+    // header.style.background = 'white';
+    // main_section.style.background = '#EAF7FC';
+
+    // central_div.style.background = 'white';
+
+
+    body.style.overflow = 'auto';
+}
+
+
+close_menu_button.addEventListener('click', close_menu)
 
 
 const button1 = document.querySelector('.slide-button-1');
@@ -34,5 +67,59 @@ button2.addEventListener("click", () => {
     left_side.style.display = 'none';
     right_side.style.display = 'flex';
 
+
+});
+
+const slider_img = document.querySelector('.slider-img');
+const next_ic = document.querySelector('.next-icon-img')
+const prev_ic = document.querySelector('.prev-icon-img')
+
+
+const lola_part1 = document.getElementById('lola-part1');
+const lola_part2 = document.getElementById('lola-part2');
+const lola_part3 = document.getElementById('lola-part3');
+const lola_part4 = document.getElementById('lola-part4');
+
+
+let images = [lola_part1, lola_part2, lola_part3, lola_part4];
+let index = 0;
+
+function Display() {
+    images.map((item, i) => {
+        if (i === index) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    })
+}
+
+function prev() {
+    if (index === 0) {
+        index = images.length - 1;
+        // return index;
+    } else {
+        index--;
+    }
+
+    Display();
+
+}
+
+function next() {
+    if (index === images.length - 1) {
+        index = 0;
+    } else {
+        index++;
+    }
+    Display();
+}
+
+prev_ic.addEventListener("click", () => {
+    prev();
+})
+
+next_ic.addEventListener("click", () => {
+    next();
 
 })
